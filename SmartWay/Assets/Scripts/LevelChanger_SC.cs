89a6e590dfb0fa37046ba0aac996df9b8d6c29f6
ProRak <ProRak25@yandex.ru>
2018-04-26 20:59:31 +0500

    новое в LevelChanger

--- SmartWay/Assets/Scripts/LevelChanger_SC.cs
+++ SmartWay/Assets/Scripts/LevelChanger_SC.cs
@@ -5,24 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger_SC : MonoBehaviour
 {
-    public GameObject currentLevel;
-    public GameObject[] allLevelPrefabs;
     public int currentLevelNumber = 0;
+    public int maxLevelNumber = 3;
 
     public void LoadNewLevel(int newLevelNumber)
     {
-        // Application.LoadLevel("1_lvl");
         SceneManager.LoadScene(newLevelNumber + "_lvl");
     }
 
     public void LoadNewLevel(string newLevelName)
     {
-        // Application.LoadLevel("1_lvl");
         SceneManager.LoadScene(newLevelName);
     }
 
     public void ReloadLevel()
     {
         LoadNewLevel(currentLevelNumber);
+    }
+
+    public void LoadNextLevel()
+    {
+        if ((currentLevelNumber + 1) > maxLevelNumber)
+        {
+            LoadNewLevel("Levels");
+        }
+        else
+        {
+            LoadNewLevel(currentLevelNumber + 1);
+        }
     }
 }
